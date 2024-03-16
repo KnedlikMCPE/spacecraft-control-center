58dcefb5ea36a13adb8d4300967cd67e7de2db78
Knedlik <knedlik@trainmazeland.com>
2024-03-16 19:06:52 +0100

    Refactor

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod script;
+mod taerobee;
+
+use krpc_client::Client;
+use script::Script;
+use taerobee::Taerobee;
+
+const ADDR: &str = "192.168.77.190";
+const PORT: u16 = 50000;
+
+fn main() {
+    let client = Client::new("Spacecraft Control Center", ADDR, PORT, PORT + 1).expect("Unable to connect.");
+
+    println!("Connected to {ADDR}:{PORT}!");
+    println!("Running scripts...");
+
+    Taerobee.run(client.clone());
+}
